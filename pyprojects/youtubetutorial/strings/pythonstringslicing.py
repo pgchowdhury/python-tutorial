d8e1c16138abb5e2ac46b86e280b0ed7b446ce9a
Prosenjit Ghosh Chowdhury <pgchowdhury@gmail.com>
2024-08-29 14:59:08 -0400

    StringSlicing
    String Slicing Tutorial in YouTube

---
+++ pyprojects/youtubetutorial/strings/pythonstringslicing.py
@@ -0,0 +1,33 @@
+"""
+    Author: Prosenjit Ghosh Chowdhury
+    Date: 27-August-2024
+"""
+greet = "HelloWorld"
+
+# # Indexing
+# print(greet[0])
+# print(greet[5])
+# print(greet[-1])
+
+# # Slicing with Positive Index
+# print(greet[5:10:1])
+# print(greet[0:5:1])
+# print(greet[0:5:2])
+
+# # Slicing with default value of Start Stop and Step parameters
+# print(greet[:10:1])
+# print(greet[5::1])
+# print(greet[5::])
+# print(greet[::])
+
+# # Reverse the String with Negative Step
+# print(greet[9:4:-1])
+# print(greet[5:10:-1])
+# print(greet[::-1])
+
+# # Slicing with negative Index
+# print(greet[-1:-6:-1])
+# print(greet[-1:-6:])
+# print(greet[-1:4:-1])
+
+
